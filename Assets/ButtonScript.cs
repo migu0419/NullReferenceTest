717a08958c5a043a9a0e599fccb03c64f5ca055c
三浦亮二 <miura@chatlune.jp>
2017-12-25 19:15:54 +0900

    FirstCommit

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	public GameObject myObject;
+	public int count;
+	public AudioClip audioClip;
+
+	void Awake () {
+		myObject = GameObject.Find ("GameObject");
+		ShowName ();
+		AddCount ();
+		myObject.GetComponent< Button> ().enabled = false;
+		if (audioClip != null) {
+			Debug.Log (audioClip.length);
+		} else {
+			Debug.Log ("Null!");
+		}
+		Debug.Log ("ほげほげ");
+	}
+	
+
+	public void ShowName(){
+		Debug.Log (myObject.name);
+	}
+
+	public void AddCount(){
+		count++;
+		Debug.Log (count);			
+	}
+}
